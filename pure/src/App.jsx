daa18faf52d5e5cc0755b158faa029ec619b3858
harsha-1807 <harshavardhan.sr@kalvium.community>
2024-03-07 12:18:05 +0530

    components created successfully

---
+++ pure/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react'
+import './App.css'
+import SimpleCounterComponent from './components/SimpleCounterComponent'
+import PureCounterComponent from './components/PureCounterComponent'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <SimpleCounterComponent/>
+        <PureCounterComponent/>
+      </div>
+    </>
+  )
+}
+
+export default App
